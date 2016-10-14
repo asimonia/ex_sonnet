b938cdbd6af8f0d1f722edb3c07b6b883f5f6fe5
asimonia <alex.simonian@gmail.com>
2016-10-13 20:40:21 -0500

    Add sonnet

---
+++ sonnet.js
@@ -0,0 +1,25 @@
+var sonnet = document.getElementById("sonnet").innerHTML;
+
+// Find and output the starting position of the word "orphans"
+console.log(sonnet.indexOf("orphans"));
+
+
+// Output the number of characters in the sonnet
+console.log(sonnet.length);
+
+
+// Replace the first occurance of the string "winter" with "yuletide"
+console.log(sonnet.replace('winter', 'yultide'));
+
+
+// replace all occurrences of the string 
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
+var newstring = replaceAll(sonnet, ' the ', ' a large ');
+
+// Set the content of the sonnet div with the new string
+
+
+
